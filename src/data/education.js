7f08278cb4f433a6fc276793bf8da45608c885b3
sadnur Islam <sadnurislam2000@gmail.com>
2025-12-06 16:50:01 +0600

    education section created

--- src/data/education.js
+++ src/data/education.js
@@ -1,16 +1,16 @@
 export const educationData = [
   {
     id: 1,
-    degree: "Bachelor of Science in Computer Science",
-    institution: "University Name",
-    location: "City, Country",
+    degree: "BSc in Computer Science & Engineering (CSE)​",
+    institution: "University of Rajshahi",
+    location: "Rajshahi, Bangladesh",
     duration: "2018 - 2022",
-    grade: "CGPA: 3.8/4.0",
+    grade: "CGPA: 3.6/4.0",
     description: "Focused on software engineering, algorithms, data structures, and web development.",
     achievements: [
-      "Dean's List for 3 consecutive semesters",
-      "Led university coding club",
-      "Winner of inter-university hackathon"
+      "Represent my varsity in ICPC Dhaka Regional 2024 and 2025",
+      "Champion in Intra-University Programming Contest multiple times",
+      "Participated in several national-level Inter-University Programming Contests (IUPC) "
     ]
   },
   // Add more education entries if needed
